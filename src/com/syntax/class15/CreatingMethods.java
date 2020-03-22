03bbc278586cd68c48f18ac5565a0fcdb2dc0f16
sabah1983 <sabahabdaullah@gmail.com>
2020-03-22 14:32:38 -0400

    New Methods

---
+++ src/com/syntax/class15/CreatingMethods.java
@@ -0,0 +1,41 @@
+package com.syntax.class15;
+
+public class CreatingMethods {
+	
+
+	//we want to create a method that will accept marks
+	//based on the marks it will return grade(A,B,C)
+	//if grade>90-->A,if 90<grade>80-->B,if 80<grade>70-->C
+	
+	char getGrade(int a) {
+		char grade;
+		if(a>90) {
+			grade='A';
+		}else if(a<=90 && a>80) {
+			grade='B';
+		}else if(a<=80 && a>70) {
+			grade='C';
+		}else {
+			grade='F';
+		}
+		return grade;
+	}
+	
+	public static void main(String[] args) {
+		CreatingMethods obj=new CreatingMethods();
+		char grade=obj.getGrade(85);
+		System.out.println(grade);
+		
+		System.out.println(obj.getGrade(65));
+		
+		// if grade is A or B---.Good job
+		//otherwise---->Study more
+		
+		if(grade=='A' || grade=='B') {
+			System.out.println("Good Job");
+		}else {
+			System.out.println("Please study more");
+		}
+	}
+
+}
